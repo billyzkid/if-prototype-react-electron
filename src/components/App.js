e8f96214ca38a62390c64d15fd620566bdff536e
Will Allan <will.allan@mlb.com>
2017-02-02 12:51:23 -0800

    Simplified

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <h1>Welcome!</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
 import App from "./components/App";
-import gridReducer from "./reducers/GridReducers";
-import "./index.css";
 
-
-const reducer = gridReducer;
-
-const configureStore = (initialState) => {
-  return createStore(reducer, initialState, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
-};
-
-const store = configureStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
